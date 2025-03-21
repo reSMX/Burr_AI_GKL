c48f3823bc068d5520f8b86ee15cdbe6e1f2a90c
SMX <142971728+reSMX@users.noreply.github.com>
2025-03-21 09:38:22 +0700

    Update ld2.py

--- backend/learn_model/ld2.py
+++ backend/learn_model/ld2.py
@@ -1,4 +1,5 @@
 import pandas as pd
+from sklearn.metrics import accuracy_score
 from sklearn.model_selection import train_test_split
 import joblib
 from catboost import CatBoostClassifier, Pool
@@ -12,9 +13,9 @@ except FileNotFoundError:
 X = df.drop('class', axis=1)
 y = df['class']
 
-model = CatBoostClassifier(iterations=3,
-                           depth=6,
-                           learning_rate=1,
+model = CatBoostClassifier(iterations=700,
+                           depth=5,
+                           learning_rate=0.01,
                            loss_function='Logloss',
                            verbose=True)
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, stratify=y)
@@ -22,8 +23,6 @@ model.fit(X_train, y_train)
 test_data = catboost_pool = Pool(X_test,
                                  y_test)
 
-# model = RandomForestClassifier(random_state=42)
-# model.fit(X_train, y_train)
 
 model_filename = 'audio_classifier_model.joblib'
 joblib.dump(model, model_filename)
@@ -34,5 +33,7 @@ print("Модель загружена.")
 
 preds_class = model.predict(test_data)
 preds_proba = model.predict_proba(test_data)
+test_accuracy = accuracy_score(y_test, preds_class)
+print(f"Точность на тестовых данных: {test_accuracy:.4f}")
 print("class = ", preds_class)
 print("proba = ", preds_proba)
